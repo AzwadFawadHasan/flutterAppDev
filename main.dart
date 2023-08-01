fe8c316f67805e53b05db7c0975400ee9bc53df3
AzwadFawadHasan2020222 <106537814+AzwadFawadHasan2020222@users.noreply.github.com>
2023-08-02 02:04:45 +0600

    scaffold widget

--- main.dart
+++ main.dart
@@ -2,6 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home:Text('hey ninjas!'),
+    home:Scaffold(
+      appBar: AppBar(
+        title: Text('This is the title'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text("THis is body text"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Text("click"),
+      ),
+    ),
   ));
 }
