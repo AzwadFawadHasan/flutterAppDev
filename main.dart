31d6a1fa95de413bfa379a9edfcc5065903d63f6
AzwadFawadHasan2020222 <106537814+AzwadFawadHasan2020222@users.noreply.github.com>
2023-08-02 17:42:34 +0600

    added images in dart

--- main.dart
+++ main.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home:Scaffold(
+void main() => runApp(MaterialApp(
+
+    home: Home(),
+
+));
+
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
       appBar: AppBar(
-        title: Text('This is the title'),
+        title: Text('Task Board'),
+
         centerTitle: true,
+        backgroundColor: Colors.black,
       ),
       body: Center(
-        child: Text("THis is body text"),
+        //child: Image.asset('assets/ss.jpg'),
+
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
-        child: Text("click"),
+        onPressed: () {},
+        child: Text("click."),
+        backgroundColor: Colors.green[800],
       ),
-    ),
-  ));
+    );
+
+  }
+
 }
